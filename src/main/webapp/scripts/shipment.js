ead887887448d3c44a8f4bc1b960d2b029076fdc
Samantha Ready <sready@salesforce.com>
2014-01-09 15:39:04 -0800

    added subscribe

--- src/main/webapp/scripts/shipment.js
+++ src/main/webapp/scripts/shipment.js
@@ -143,7 +143,7 @@
                         //$$.client.publish(sr.client, {name : 'publisher.setValidForSubmit', payload : true});
 
                         // Target a specific canvas app
-                        $$.controller.publish({name : 'mynamespace.myevent1', payload : {}, target : {}});
+                        $$.client.publish({name : 'mynamespace.myevent1', payload : {}, target : {}});
 
                     }
                 });
